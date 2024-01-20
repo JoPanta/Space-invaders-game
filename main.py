009f6d8932c42ebd9a19d981db31f9286db6b199
João Pantaleão <jopantaleao@gmail.com>
2024-01-20 23:30:05 +0000

    done

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ pygame.display.set_caption("Space Invaders")
 WHITE = (255, 255, 255)
 
 SCORE_FONT = pygame.font.SysFont('Times New Roman', 30)
+GAME_OVER_FONT = pygame.font.SysFont('Times New Roman', 80)
+VICTORY_FONT = pygame.font.SysFont('Times New Roman', 80)
 
 FPS = 60
 VEL = 5
@@ -24,7 +26,17 @@ SPACESHIP = pygame.transform.scale(SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_
 
 BULLET_IMAGE = pygame.image.load('Assets/bullet.png')
 BULLET = pygame.transform.scale(BULLET_IMAGE, (8, 16))
+
+
 BULLET_SOUND = pygame.mixer.Sound('Assets/Gun+Silencer.mp3')
+BULLET_SOUND.set_volume(0.2)
+VICTORY_SOUND = pygame.mixer.Sound('Assets/victory.mp3')
+VICTORY_SOUND.set_volume(0.2)
+DEATH_SOUND = pygame.mixer.Sound('Assets/explosion.mp3')
+DEATH_SOUND.set_volume(0.2)
+ENEMY_BULLET_SOUND = pygame.mixer.Sound('Assets/laser.mp3')
+ENEMY_BULLET_SOUND.set_volume(0.2)
+GAME_OVER_SOUND = pygame.mixer.Sound('Assets/gameover.mp3')
 
 ENEMY_BULLET = pygame.transform.scale(pygame.image.load('Assets/enemybullet.png'), (8, 16))
 
@@ -32,13 +44,15 @@ ALIEN = pygame.transform.scale(pygame.image.load('Assets/alien.png'), (ALIEN_WID
 
 SPACE = pygame.transform.scale(pygame.image.load('Assets/space.png'), (WIDTH, HEIGHT))
 
+HEART = pygame.transform.scale(pygame.image.load('Assets/heart.png'), (40, 25))
+
 ALIEN_HIT = pygame.USEREVENT + 1
 ALIEN_MOVE_EVENT = pygame.USEREVENT + 2
 ALIEN_MOVE_INTERVAL = 500
 PLAYER_HIT = pygame.USEREVENT + 3
 
 
-def draw_window(player, bullets, aliens, score, enemy_bullets):
+def draw_window(player, bullets, aliens, score, enemy_bullets, lives):
 
     WIN.blit(SPACE, (0, 0))
 
@@ -48,9 +62,13 @@ def draw_window(player, bullets, aliens, score, enemy_bullets):
     WIN.blit(score_text, (10, HEIGHT - 40))
 
 
+
     # Calling ALIENS
     for alien in aliens:
         WIN.blit(ALIEN, (alien.x, alien.y))
+
+    for live in lives:
+        WIN.blit(HEART, (live.x, live.y))
 
     for bullet in bullets:
         WIN.blit(BULLET, (bullet.x, bullet.y))
@@ -68,9 +86,25 @@ def spaceship_movement(keys_pressed, player):
         player.x += VEL
 
 
+ALIEN_DIRECTION = 1
+
+
 def alien_movement(aliens):
+    global ALIEN_DIRECTION
+
     for alien in aliens:
         alien.y += ALIEN_VEL
+
+    if ALIEN_DIRECTION == 1:
+        for alien in aliens:
+            alien.x += ALIEN_VEL
+        ALIEN_DIRECTION = -1
+    else:
+        for alien in aliens:
+            alien.x -= ALIEN_VEL
+        ALIEN_DIRECTION = 1
+
+
 
 
 
@@ -83,7 +117,7 @@ def bullet_movement(bullets, aliens):
         bullet.y -= BULLET_VEL
 
         for alien in aliens:
-            if alien.colliderect(bullet):
+            if alien.colliderect(bullet) and alien.y > -15:
                 bullets.remove(bullet)
                 pygame.event.post(pygame.event.Event(ALIEN_HIT))
                 aliens_to_remove.append(alien)
@@ -93,9 +127,12 @@ def bullet_movement(bullets, aliens):
 
 
 def create_enemy_bullet(aliens):
-    random_alien = random.choice(aliens)
-    bullet = pygame.Rect(random_alien.x, random_alien.y, 10, 5)
-    return bullet
+    if len(aliens) > 0:
+        random_alien = random.choice(aliens)
+        bullet = pygame.Rect(random_alien.x, random_alien.y, 10, 5)
+        return bullet
+    else:
+        pass
 
 
 
@@ -107,6 +144,20 @@ def enemy_bullets_movement(enemy_bullets, player):
             enemy_bullets.remove(bullet)
             pygame.event.post(pygame.event.Event(PLAYER_HIT))
 
+
+def draw_game_over():
+    game_over_text = GAME_OVER_FONT.render('GAME OVER', 1, WHITE)
+    WIN.blit(game_over_text, (WIDTH//2 - game_over_text.get_width()//2, HEIGHT//2 - game_over_text.get_height()//2))
+    pygame.display.update()
+    pygame.time.delay(3000)
+
+
+def draw_victory():
+    VICTORY_SOUND.play()
+    victory_text = VICTORY_FONT.render('YOU WIN!', 1, WHITE)
+    WIN.blit(victory_text, (WIDTH//2 - victory_text.get_width()//2, HEIGHT//2 - victory_text.get_height()//2))
+    pygame.display.update()
+    pygame.time.delay(3000)
 
 
 def main():
@@ -130,7 +181,8 @@ def main():
 
 
     for i in range(3):
-        live = pygame.Rect(400, 750, 40, 25)
+        x = 45 * i
+        live = pygame.Rect(350 + x, 760, 40, 25)
         lives.append(live)
 
     run = True
@@ -139,6 +191,14 @@ def main():
 
     while run:
         clock.tick(FPS)
+
+        for alien in aliens:
+            if alien.y > HEIGHT - 130:
+                GAME_OVER_SOUND.play()
+                draw_game_over()
+                run = False
+                break
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -148,22 +208,41 @@ def main():
                 if event.key == pygame.K_SPACE:
                     bullet = pygame.Rect(player.x + player.width//2 - 2, player.y, 10, 5)
                     bullets.append(bullet)
-                    # BULLET_SOUND.play()
+                    BULLET_SOUND.play()
 
             if event.type == ALIEN_HIT:
                 score += 100
+                if len(aliens) == 0:
+                    draw_victory()
+                    run = False
+                    break
 
             if event.type == ALIEN_MOVE_EVENT:
                 alien_movement(aliens)
 
-                if random.randint(1, 2) == 1:
+                if random.randint(1, 1) == 1:
                     enemy_bullets.append(create_enemy_bullet(aliens))
+                    ENEMY_BULLET_SOUND.play()
+
+            if event.type == PLAYER_HIT:
+                if len(lives) > 1:
+
+                    DEATH_SOUND.play()
+                    lives.pop()
+
+                else:
+                    GAME_OVER_SOUND.play()
+                    draw_game_over()
+                    run = False
+                    break
+
 
         keys_pressed = pygame.key.get_pressed()
         spaceship_movement(keys_pressed, player)
         bullet_movement(bullets, aliens)
         enemy_bullets_movement(enemy_bullets, player)
-        draw_window(player, bullets, aliens, score, enemy_bullets)
+        draw_window(player, bullets, aliens, score, enemy_bullets, lives)
+    main()
 
 
 
